8bb09cec3b426226860217ca63a3d555209d9b84
Maksim Yegorov <myegorov@twitter.com>
2018-12-06 17:56:52 -0800

    deploying

--- app/app.py
+++ app/app.py
@@ -30,19 +30,28 @@ def create_app():
 
     @app.route('/upload', methods = ['POST'])
     def upload():
-        img = Image.open(request.files['photo'].stream)
-        print("NOTE: request.get_json():", request.get_json(), '\n\n')
-        print("NOTE: request.data:", request.data, '\n\n')
-        print("NOTE: request.headers", request.headers, '\n\n')
-        print('NOTE: request.__dict__:', request.__dict__, '\n\n')
-        print('NOTE: request.form:', request.form, '\n\n')
-        print('NOTE: request.args:', request.args, '\n\n')
-        print('NOTE: request.files:', request.files, '\n\n')
-        print('NOTE: request.values:', request.values, '\n\n')
-        sys.stdout.flush()
-        message = ocr(img)
-        resp = jsonify(message)
-        resp.status_code = 200
+        if 'multipart' in request.headers['Content-Type']:
+            img = Image.open(request.files['photo'].stream)
+#            print("NOTE: request.get_json():", request.get_json(), '\n\n')
+#            print("NOTE: request.data:", request.data, '\n\n')
+#            print("NOTE: request.headers", request.headers, '\n\n')
+#            print('NOTE: request.__dict__:', request.__dict__, '\n\n')
+#            print('NOTE: request.form:', request.form, '\n\n')
+#            print('NOTE: request.args:', request.args, '\n\n')
+#            print('NOTE: request.files:', request.files, '\n\n')
+#            print('NOTE: request.values:', request.values, '\n\n')
+#            sys.stdout.flush()
+            message = ocr(img)
+            resp = jsonify(message)
+            resp.status_code = message['status']
+        else:
+            message = {
+                    'status': 415,
+                    'caption': 'Unsupported media type',
+                    'url': img_path
+            }
+            resp = jsonify(message)
+            resp.status_code = 415
 
         return resp
 
