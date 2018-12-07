7beecf9c171d91d9e47e09bbe9647936aaec8161
Maksim Yegorov <myegorov@twitter.com>
2018-12-06 16:12:11 -0800

    flush log

--- app/app.py
+++ app/app.py
@@ -29,8 +29,15 @@ def create_app():
 
     @app.route('/upload', methods = ['POST'])
     def upload():
-        print("NOTE: request.headers", request.headers)
-        print('NOTE: request.__dict__:', request.__dict__)
+        print("NOTE: request.get_json():", request.get_json(), '\n\n')
+        print("NOTE: request.data:", request.data, '\n\n')
+        print("NOTE: request.headers", request.headers, '\n\n')
+        print('NOTE: request.__dict__:', request.__dict__, '\n\n')
+        print('NOTE: request.form:', request.form, '\n\n')
+        print('NOTE: request.args:', request.args, '\n\n')
+        print('NOTE: request.files:', request.files, '\n\n')
+        print('NOTE: request.values:', request.values, '\n\n')
+
         sys.stdout.flush()
 
         message = {
